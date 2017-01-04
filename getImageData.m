bc291193f826382c185e4d2d14d1629ee0cdd463
hepcat72 <hepcat72@gmail.com>
2017-01-04 17:07:39 -0500

    This method has been rendered defunct because it cannot work to record and analyze the camera images at the same time.

--- getImageData.m
+++ getImageData.m
@@ -1,16 +1,35 @@
 function getImageData(obj,event)
 
-  [data, time, metadata] = getdata(obj,obj.FramesAcquiredFcnCount,'native','cell');
-  %Save the last frame as the second element in the UserData cell array
-  ca = obj.UserData;
-  ca{3} = ca{3} + 1;
-  disp(['Recording frame [' num2str(ca{3}) ']'])
-  obj.UserData = {ca{1} data{1} ca{3}};
-  %tmp = obj.UserData(1);
-  %set(obj,'UserData',[tmp,(data)]);
+  [data,time,metadata] = getdata(obj,obj.FramesAcquiredFcnCount,'native','cell');
+
+  %Get & update the user data
+  ud           = obj.UserData;
+  fid          = ud{1};
+  %frameCount   = ud{2} + 1;
+  %lastFramePos = ud{3};
+  %nextFramePos = ud{4};
+
+  %fid = obj.UserData(1);
+  %obj.UserData(2) = obj.UserData(2) + 1;
+
+  %Save the user data changes
+  %ud{2}            = frameCount;
+  %ud{3}            = nextFramePos;
+  %ud{4}            = nextFramePos + 1;
+  %ud{nextFramePos} = data{1};
+  %obj.UserData     = ud;
+  %obj.UserData = {ud{1} frameCount newFramePos data{1}};
+  %obj.UserData = {fid frameCount};
+
+  %disp(['Recording frame [' num2str(frameCount) ']'])
+  disp(['Recording a frame'])
+  
+  %set(obj,'UserData',ud);
+
   for ii = 1:numel(metadata)
-    frameTime = datetime(datenum(metadata(ii).AbsTime), 'ConvertFrom', 'datenum', 'Format', 'dd-MMM-uuuu HH:mm:ss.SSSSSSSSS');
-    fprintf(ca{1},'%s\r\n',frameTime);
+    frameTime = datetime(datenum(metadata(ii).AbsTime),'ConvertFrom',...
+                         'datenum','Format','dd-MMM-uuuu HH:mm:ss.SSSSSSSSS');
+    fprintf(fid,'%s\r\n',frameTime);
   end
 
 end
